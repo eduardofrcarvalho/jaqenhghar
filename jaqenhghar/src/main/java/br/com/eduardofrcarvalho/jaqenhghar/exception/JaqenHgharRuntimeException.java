61c584795bcc6bf5692ad9f903489f61e28cd171
Eduardo de Freitas Carvalho <eduardo.frcarvalho@gmail.com>
2020-06-29 19:45:59 -0300

    Atualização na classe de exceção do componente. Foram implementados construtores da classe.

--- jaqenhghar/src/main/java/br/com/eduardofrcarvalho/jaqenhghar/exception/JaqenHgharRuntimeException.java
+++ jaqenhghar/src/main/java/br/com/eduardofrcarvalho/jaqenhghar/exception/JaqenHgharRuntimeException.java
@@ -1,6 +1,24 @@
 package br.com.eduardofrcarvalho.jaqenhghar.exception;
 
+import lombok.Getter;
 
 public class JaqenHgharRuntimeException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
+	@Getter
+	private String code;
+	
+	@Getter
+	private String[] parameters;
+	
+	public JaqenHgharRuntimeException(String pCode, String... pParameters) {
+		this(null, pCode, pParameters);
+	}
+
+	public JaqenHgharRuntimeException(Throwable pThrowable, String pCode, String... pParameters) {
+		super(pThrowable);
+		this.code = pCode;
+		this.parameters = pParameters;
+	}
 }
